421de6f5989c5e77b098b1acdfadf03e728c3139
Ryo <21cm0108@jec.ac.jp>
2022-06-14 10:07:02 +0900

    Initial Commit

---
+++ SilentCamera/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SilentCamera
+//
+//  Created by Ryo on 2022/06/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SilentCamera/SilentCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  SilentCameraApp.swift
+//  SilentCamera
+//
+//  Created by Ryo on 2022/06/14.
+//
+
+import SwiftUI
+
+@main
+struct SilentCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
